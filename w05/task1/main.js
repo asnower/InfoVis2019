6b49cd1e45ef670f4df40cc9ad4aa84b0f16a882
asnower <49426814+asnower@users.noreply.github.com>
2019-05-14 13:54:59 +0900

    Update main.js

--- w05/task1/main.js
+++ w05/task1/main.js
@@ -1,16 +1,13 @@
 function main()
 {
-    var width = 1000;
-    var height = 1000;
-
     var scene = new THREE.Scene();
 
     var fov = 45;
     var aspect = width / height;
     var near = 1;
     var far = 1000;
-    var camera = new THREE.PerspectiveCamera( fov, aspect, near, far );
-    camera.position.set( 0, 0, 5 );
+    var camera = new THREE.PerspectiveCamera( 75, window.innerWidth/window.innerHeight, 0.1, 1000 );
+
     scene.add( camera );
 
     var light = new THREE.PointLight();
@@ -22,29 +19,29 @@ function main()
     document.body.appendChild( renderer.domElement );
 
     var vertices = [
-        [ -1, -1, -1 ], // 0
-        [  1, -1, -1 ], // 1
-        [  1, -1,  1 ], // 2
-        [ -1, -1,  1 ], // 3
-        [ -1,  1, -1 ], // 4
-        [  1,  1, -1 ], // 5
-        [  1,  1,  1 ], // 6
-        [ -1,  1,  1 ]  // 7
+        [0, 0, 0],
+        [0, 0, 1],
+        [0, 1, 0],
+        [0, 1, 1],
+        [1, 0, 1],
+        [1, 0, 0],
+        [1, 1, 1],
+        [1, 1, 0],
     ];
 
     var faces = [
-        [ 0, 1, 2 ], // f0
-        [ 0, 2, 3 ], // f1
-        [ 7, 6, 5 ], // f2  
-        [ 7, 5, 4 ], // f3
-        [ 0, 4, 1 ], // f4
-        [ 1, 4, 5 ], // f5
-        [ 1, 5, 6 ], // f6
-        [ 1, 6, 2 ], // f7
-        [ 2, 6, 3 ], // f8
-        [ 3, 6, 7 ], // f9
-        [ 0, 3, 7 ], // f10
-        [ 0, 7, 4 ], // f11
+        [0, 1, 2],
+        [1, 3, 2],
+        [4, 5, 6],
+        [5, 7, 6],
+        [0, 5, 1],
+        [5, 4, 1],
+        [3, 6, 2],
+        [6, 7, 2],
+        [5, 0, 2],
+        [5, 2, 7],
+        [1, 4, 3],
+        [4, 6, 3],
     ];
 
     var geometry = new THREE.Geometry();
