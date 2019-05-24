54320ab4e6b9c29facd313794ca0a71cd96269af
asnower <49426814+asnower@users.noreply.github.com>
2019-05-24 20:10:30 +0900

    Update main.js

--- myTest/w04/main.js
+++ myTest/w04/main.js
@@ -1,21 +1,9 @@
 function main()
 {
-    var width = 1000;
-    var height = 1000;
+    var width = 500;
+    var height = 500;
 
     var scene = new THREE.Scene();
-
-    var light = new THREE.PointLight(0xffffff);
-    light.position.set(1,1,1);
-    scene.add(light);
-
-    var light2 = new THREE.PointLight(0xffffff,0.3);
-    light2.position.set(-1,-1,-1);
-    scene.add(light2);
-
-    var directionalLight = new THREE.DirectionalLight(0xffffff, 1.0);
-    directionalLight.position.set(THREE.Object3D.DefaultDown);
-    //scene.add(directionalLight);
 
     var fov = 45;
     var aspect = width / height;
@@ -29,8 +17,8 @@ function main()
     renderer.setSize( width, height );
     document.body.appendChild( renderer.domElement );
 
-    var geometry = new THREE.BoxGeometry( 10, 10, 10 );
-    var material = new THREE.MeshLambertMaterial({color: 0x111111});
+    var geometry = new THREE.BoxGeometry( 1, 1, 1 );
+    var material = new THREE.MeshBasicMaterial( { color: 0xffffff } );
     var cube = new THREE.Mesh( geometry, material );
     scene.add( cube );
 
@@ -39,8 +27,8 @@ function main()
     function loop()
     {
         requestAnimationFrame( loop );
-        cube.rotation.x += 0.01;
-        cube.rotation.y += 0.01;
+        cube.rotation.x += 0.001;
+        cube.rotation.y += 0.001;
         renderer.render( scene, camera );
     }
 }
