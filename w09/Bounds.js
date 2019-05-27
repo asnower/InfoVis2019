86e1c1685df20fa13e6cc03b0e81e158f686bb83
asnower <49426814+asnower@users.noreply.github.com>
2019-05-27 17:54:52 +0900

    Update Bounds.js

--- w09/Bounds.js
+++ w09/Bounds.js
@@ -34,7 +34,7 @@ function Bounds( volume )
     geometry.vertices.push( v3 ); geometry.vertices.push( v7 );
 
     material.linewidth = 2;
-    material.color = new THREE.Color( "green" );
+    material.color = new THREE.Color( "gold" );
 
     return new THREE.Line( geometry, material, THREE.LinePieces );
 }
